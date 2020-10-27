09c1c235d5f4859be55b51a04f3603998a6b89b0
Mohsen Bostan <mohsenbostandev@gmail.com>
2020-10-27 16:09:05 +0330

    Complete new create thread

--- src/requests/Threads.js
+++ src/requests/Threads.js
@@ -3,3 +3,7 @@ import Axios from "../axios";
 export let threadsListRequest = (page) => {
     return Axios.get(`threads?page=${page}`)
 };
+
+export let createNewThreadRequest = (formData) => {
+    return Axios.post(`threads`, formData)
+};
--- src/views/thread/CreateThread.vue
+++ src/views/thread/CreateThread.vue
@@ -1,13 +1,13 @@
 <template>
   <v-container>
     <v-row
-      justify="center"
-      content="center"
-      >
+        justify="center"
+        content="center"
+    >
 
       <v-col
-      cols="12"
-      md="8"
+          cols="12"
+          md="8"
       >
         <v-card>
           <v-card-title>
@@ -18,6 +18,7 @@
               <v-text-field
                   label="Title"
                   name="title"
+                  v-model="formData.title"
                   type="text"
                   outlined
               ></v-text-field>
@@ -25,11 +26,16 @@
               <v-textarea
                   outlined
                   name="content"
+                  v-model="formData.content"
                   label="Content"
                   value=""
               ></v-textarea>
 
               <v-select
+                  v-model="formData.channel_id"
+                  :items="channels"
+                  item-text="name"
+                  item-value="id"
                   label="Channel"
                   outlined
               ></v-select>
@@ -37,9 +43,10 @@
           </v-card-text>
           <v-card-actions>
             <v-btn
-            ripple
-            color="pink"
-            dark
+                ripple
+                color="pink"
+                dark
+                @click="createThread"
             >
               Submit
             </v-btn>
@@ -52,8 +59,34 @@
 </template>
 
 <script>
+import {channelsListRequest} from "@/requests/Channels";
+import {createNewThreadRequest} from "@/requests/Threads";
+
 export default {
-name: "CreateThread"
+  name: "CreateThread",
+  data: () => ({
+    formData: {
+      title: null,
+      content: null,
+      channel_id: null,
+    },
+    channels: null,
+  }),
+  methods: {
+    fetchChannelsList() {
+      channelsListRequest().then(res => {
+        this.channels = res.data
+      })
+    },
+    createThread() {
+      createNewThreadRequest(this.formData).then(res => {
+        this.$router.push('/')
+      })
+    }
+  },
+  mounted() {
+    this.fetchChannelsList()
+  }
 }
 </script>
 
